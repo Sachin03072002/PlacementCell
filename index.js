7e8ce6e53b845afc5906b82500949ec99d5ad63b
Sachin <sachinmondal720@gmail.com>
2023-06-29 12:05:20 +0530

    added virtual port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const port = 8000;
+const port = process.env.port || 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
